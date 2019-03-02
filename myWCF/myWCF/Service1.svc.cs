c3a86f00b92135d6c5b42e54f7efd765afc01f5c
syed Ikram Ali <syed.ikramali@gmail.com>
2019-03-02 17:48:36 -0500

    05

--- myWCF/myWCF/Service1.svc.cs
+++ myWCF/myWCF/Service1.svc.cs
@@ -6,6 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
+//05
 namespace myWCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
