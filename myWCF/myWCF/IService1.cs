d468fc5ca854078e33ad3e8242faf436a4b55ee2
syed Ikram Ali <syed.ikramali@gmail.com>
2019-03-02 18:01:03 -0500

    101

--- myWCF/myWCF/IService1.cs
+++ myWCF/myWCF/IService1.cs
@@ -6,10 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-//Dear Sir Jee
-//Hello sir 2
-//3
-//4
+//101
 namespace myWCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
