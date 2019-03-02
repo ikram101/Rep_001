d625bb56f6f6e3dd35299e7f669ea6adea4143e7
syed Ikram Ali <syed.ikramali@gmail.com>
2019-03-02 15:21:53 -0500

    02 after merge

--- myWCF/myWCF/Service1.svc.cs
+++ myWCF/myWCF/Service1.svc.cs
@@ -7,6 +7,7 @@ using System.ServiceModel.Web;
 using System.Text;
 
 // 01 after merge
+//02 after merge
 namespace myWCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
